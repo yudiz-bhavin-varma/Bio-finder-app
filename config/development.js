ac5f8f2b088a5b5d93324a90d9cb433aff1ddac1
varma4512 <bhavin.verma@yudiz.com>
2023-07-26 22:38:47 +0530

    code improvement

--- config/development.js
+++ config/development.js
@@ -1,10 +1,9 @@
 const dev = {
   DEPLOY_HOST_URL: process.env.DEPLOY_HOST_URL,
   PORT: process.env.PORT || 1338,
-  DB_URL:'mongodb+srv://cokobij614:lJwIgeAaVMa8zyEc@bio-finder-cluster.ozsfruy.mongodb.net/',
-  host: "redis-11023.c264.ap-south-1-1.ec2.cloud.redislabs.com",
-  port: "11023",
-  password: "nmC1nPlZFAU73eFylmn1M9uk8FwzIbzt"
+  DB_URL: process.env.DB_URL,
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD,
 }
-
 module.exports = dev
--- config/production.js
+++ config/production.js
@@ -1,6 +1,10 @@
 const prod = {
   DEPLOY_HOST_URL: process.env.DEPLOY_HOST_URL,
-  PORT: process.env.PORT || 1338
+  PORT: process.env.PORT || 1338,
+  DB_URL: process.env.DB_URL,
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD,
 }
 
 module.exports = prod
