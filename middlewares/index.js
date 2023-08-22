1b98d5721f6ed4eadc3af1e1ed1918b4fcd98704
varma4512 <bhavin.verma@yudiz.com>
2023-08-22 15:48:02 +0530

    cors configuration added

--- middlewares/index.js
+++ middlewares/index.js
@@ -15,8 +15,7 @@ module.exports = (app) => {
 
   const corsConfig = {
     origin: '*',
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
+    methods: '*'
   }
 
   app.use(cors(corsConfig))
